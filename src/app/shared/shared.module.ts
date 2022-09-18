00f1fe5e168243a178c1f3c73fdea8f7e406a366
malekhannachi <malek.hannachi05@gmail.com>
2022-09-18 10:26:45 +0100

    setup angular material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { MatrialModule } from './matrial/matrial.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, BrowserModule, RouterModule],
-  exports: [HeaderComponent, CommonModule, BrowserModule, RouterModule],
+  imports: [CommonModule, BrowserModule, RouterModule, MatrialModule],
+  exports: [
+    HeaderComponent,
+    CommonModule,
+    BrowserModule,
+    RouterModule,
+    MatrialModule,
+  ],
 })
 export class SharedModule {}
